beec2107c6fc18b43e409e9512a4f770c2a0ee48
Nyagami <hoangquan05112002@gmail.com>
2023-05-16 10:23:48 +0700

    feat: model (MVC like)

---
+++ src/database/types/model.ts
@@ -0,0 +1,61 @@
+import { NovelStatus } from '@plugins/types';
+
+export interface Category {
+  id: number;
+  name: string;
+  sort: number;
+}
+
+export interface Novel {
+  id: number;
+  url: string;
+  pluginId: string;
+  categories: Category[];
+  name: string;
+  cover?: string;
+  summary?: string;
+  author?: string;
+  artist?: string;
+  status?: NovelStatus;
+  genres?: string;
+  inLibrary: number;
+}
+
+export interface Chapter {
+  id: number;
+  novel: Novel;
+  url: string;
+  name: string;
+  releaseTime?: string;
+  readTime: string;
+  bookmark: number;
+  unread: number;
+  isDownloaded: number;
+}
+
+export interface DownloadedChapter {
+  id: number;
+  chapter: Chapter;
+  chapterText: string;
+}
+
+export interface History {
+  chapter: Chapter;
+  novel: Novel;
+}
+
+export interface LibraryNovel extends Novel {
+  chaptersUnread: number;
+  chaptersDownloaded: number;
+}
+
+export interface LibraryStats {
+  novelsCount?: number;
+  chaptersCount?: number;
+  chaptersRead?: number;
+  chaptersUnread?: number;
+  chaptersDownloaded?: number;
+  sourcesCount?: number;
+  genres?: Record<string, number>;
+  status?: Record<string, number>;
+}
